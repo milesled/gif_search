85cc08375c85e4c36f11993e96be8f5a58726890
Miles Lederman <miles@D-10-19-197-204.dhcp4.washington.edu>
2019-07-24 10:46:04 -0700

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Search from '@material-ui/icons/Search';
 
 class App extends React.Component {
 
@@ -17,7 +21,7 @@ class App extends React.Component {
     var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
     var r = await fetch(url)
     var json = await r.json()
-    this.setState({ memes: json.data, loading: false, text: ' ' })
+    this.setState({ memes: json.data, loading: false, text: '' })
   }
 
   render() {
@@ -25,14 +29,28 @@ class App extends React.Component {
     return (
       <div className="App">
         <form className="App-header" onSubmit={this.getMemes}>
-          <input value={text}
-            onChange={e => this.setState({ text: e.target.value })} />
-          <button disabled={loading || !text} type="submit">
+          <TextField 
+            id="outlined-search"
+            label="Search for GIFs"
+            type="search"
+            margin="normal"
+            variant="outlined"
+            value={text}
+            onChange={e => this.setState({ text: e.target.value })} 
+            style = {{width: '90%', height: 55, margin: 10}}/>
+          <Button 
+            color="primary"
+            variant="contained"
+            disabled={loading || !text} 
+            type="submit"
+            style = {{height: 55, width: '10%', marginRight: 10}}>
+            <Search />
             Search
-          </button>
+          </Button>
         </form>
+        { !loading || <LinearProgress /> }
         {memes.map(meme => {
-          return <Meme key={meme.id} meme={meme} />
+          return <Meme key={meme.id} meme={meme} /> 
         })}
       </div>
     );
